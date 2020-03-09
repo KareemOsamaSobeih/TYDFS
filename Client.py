8bfa67cdae40f48b2b986d4b0e29731351ef2266
Muhammad Sayed <muhammad.mahmoud98@eng-st.cu.edu.eg>
2020-03-10 01:14:37 +0200

    add connection between data keeper and client for downloading a single chunck file and test it

---
+++ Client.py
@@ -0,0 +1,16 @@
+import zmq
+
+def download(filePath, addresses):
+    context = zmq.Context()
+    downSocket = context.socket(zmq.PULL)
+    for address in addresses:
+        downSocket.connect("tcp://%s:%s"%(address[0], address[1]))
+    data = downSocket.recv()
+    with open(filePath, "wb") as file:
+        file.write(data)
+
+    for address in addresses:
+        downSocket.disconnect("tcp://%s:%s"%(address[0], address[1]))
+    downSocket.close()
+        
+download('data/Client/test1.mp4', [('127.0.0.1', '5000')])
---
+++ DataKeeper.py
@@ -0,0 +1,14 @@
+import zmq
+ipAddress = '127.0.0.1'
+port = '5000'
+
+def send(filePath):
+    context = zmq.Context()
+    upSocket = context.socket(zmq.PUSH)
+    upSocket.bind("tcp://%s:%s"%(ipAddress, port))
+    with open(filePath, "rb") as file:
+        upSocket.send(file.read())
+    upSocket.close()
+
+
+send("data/DataKeeper/SampleVideo.mp4")
